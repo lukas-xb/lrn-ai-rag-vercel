6b7fed7e947638223a9dea5bcfeba83152d5488f
lukas-xb <lukas@lukasbajer.cz>
2025-07-05 09:10:33 +0200

    Refines system prompt to only use knowledge from the database

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,6 +9,7 @@ export async function POST(req: Request) {
 
     const result = await streamText({
         model,
+        system: `You are a helpful assistant. Check your knowledge base before answering any questions. Only respond to questions using information from tool calls. if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
         messages: messages,
     })
     return result.toDataStreamResponse();
