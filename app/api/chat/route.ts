f1d10dbef4d050c35c097dcb83455321472b54d2
lukas-xb <lukas@lukasbajer.cz>
2025-07-05 09:25:42 +0200

    Defines addResource tools to work with the knowledge base

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,7 @@
 import { createOllama } from "ollama-ai-provider"
-import { streamText, UIMessage } from "ai"
+import { streamText, UIMessage, tool } from "ai"
+import { z } from "zod"
+import { createResource } from "@/lib/actions/resources";
 
 const ollama = createOllama();
 const model = ollama("gemma3n")
@@ -11,6 +13,15 @@ export async function POST(req: Request) {
         model,
         system: `You are a helpful assistant. Check your knowledge base before answering any questions. Only respond to questions using information from tool calls. if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
         messages: messages,
+        tools: {
+            addResource: tool({
+                description: "add a resource to your knowledge base. If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation",
+                parameters: z.object({
+                    content: z.string().describe("the content or resource to add to the knowledge base")
+                }),
+                execute: async ({ content }) => createResource({ content }),
+            }),
+        }
     })
     return result.toDataStreamResponse();
 }
