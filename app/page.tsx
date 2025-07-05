a9b0074c24d40dd8655a50c6234937db16715258
lukas-xb <lukas@lukasbajer.cz>
2025-07-05 08:08:09 +0200

    Adds page configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
+"use-client";
+
+import { useChat } from "@ai-sdk/react";
+
+
 export default function Chat() {
-  return <main>Hello, world!</main>;
+  const { messages, input, handleInputChange, handleSubmit } = useChat({})
+
+  return <main>
+    <div>
+      {messages.map(message => (
+        <div key={message.id}>
+          <div>
+            <div>{message.role}</div>
+            <p>{message.content}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Type your message here..."
+      />
+      <button type="submit">Send</button>
+    </form>
+  </main>;
 }
