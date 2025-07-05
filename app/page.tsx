66bbbeac0ee8ff593a7aaf1b10ae146c3f237992
lukas-xb <lukas@lukasbajer.cz>
2025-07-05 08:16:01 +0200

    Adds basic styling

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,24 @@ import { useChat } from "@ai-sdk/react";
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({})
 
-  return <main>
-    <div>
+  return <main className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="space-y-4">
       {messages.map(message => (
         <div key={message.id}>
-          <div>
-            <div>{message.role}</div>
+          <div className="border p-4 rounded-md">
+            <div className="font-bold">{message.role}</div>
             <p>{message.content}</p>
           </div>
         </div>
       ))}
     </div>
     <form onSubmit={handleSubmit}>
-      <input
+      <input className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl bg-white"
         type="text"
         value={input}
         onChange={handleInputChange}
         placeholder="Type your message here..."
       />
-      <button type="submit">Send</button>
     </form>
   </main>;
 }
